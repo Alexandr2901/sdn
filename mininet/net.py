ce34913558c1d9abb7b4cf33daa02a3f4b5a3619
alexandr <zitov01@mail.ru>
2022-10-22 09:35:30 +0300

    stage nginx

--- mininet/net.py
+++ mininet/net.py
@@ -8,9 +8,13 @@ from mininet.link import Link
 
 
 def emptyNet():
-
     net = Mininet(controller=RemoteController, waitConnected=True)
-    net.addController('co1',controller=RemoteController, ip="opendaylight1",port=6633)
+    # net.addController('co1',controller=RemoteController, ip="opendaylight1",port=6633)
+    # c0 = net.addController('c0',controller=RemoteController, ip="nginx",port=6633)
+    c1 = net.addController('c1',controller=RemoteController, ip="opendaylight1",port=6633)
+    # c2 = net.addController('c2',controller=RemoteController, ip="opendaylight2",port=6633)
+    # c3 = net.addController('c3',controller=RemoteController, ip="opendaylight3",port=6633)
+
     # net.addController('co2',controller=RemoteController, ip="opendaylight2",port=6633)
     # net.addController('co3',controller=RemoteController, ip="opendaylight3",port=6633)
 
@@ -27,12 +31,29 @@ def emptyNet():
     net.addLink(S2,S1)
     net.addLink(S3,S1)
     net.addLink(S4,S1)
+
+    net.addLink(S2,S3)
+    net.addLink(S3,S4)
+    net.addLink(S4,S2)
+
+    # net.build()
+   
     net.start()
+    # SC = net.addSwitch('s5')
+    # net.addLink(SC,S1)
+    # SC.start([c1,c2,c3])
+    # S1.start()
+    # S2.start()
+    # S3.start()
+    # S4.start()
+    # net.addLink(SC,c1)
+   
+    # 
     net.pingAll()
-    for i in range(50):
+    for i in range(1):
         S1 = net.get('s1')
-        Sn = net.addSwitch('s' + str(i+5))
-        Hn = net.addHost('h' + str(i+5))
+        Sn = net.addSwitch('s' + str(i+6))
+        Hn = net.addHost('h' + str(i+6))
         hlink = net.addLink(Hn,Sn)
         slink = net.addLink(S1,Sn)
         S1.attach(slink.intf1)
@@ -41,10 +62,10 @@ def emptyNet():
         Sn.attach(hlink.intf1)
         Hn.configDefault(defaultRoute=Hn.defaultIntf())
         net.pingAll()
-        sleep(5)
-    for i in range(1000):
-        net.pingAll()
-        sleep(5)
+        sleep(1)
+    # for i in range(1000):
+    #     net.pingAll()
+    #     sleep(5)
     CLI(net)
     net.stop()
 if __name__ == '__main__':
