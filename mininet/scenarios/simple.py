283d9c27e97fc4385ad04c3e19d280cd848a7504
Aleksandr Zhitov <zitov01@mail.ru>
2023-03-03 13:18:15 +0300

    stages

--- mininet/scenarios/simple.py
+++ mininet/scenarios/simple.py
@@ -11,17 +11,21 @@ import os
 def emptyNet():
     net = Mininet(controller=RemoteController, waitConnected=True)
     S1 = net.addSwitch('s1')
-    
     fileName = 'simple.pcap'
-    os.remove(fileName)
-    s1_pcap = S1.popen('tcpdump -w '+fileName+' -i any')
+    try:
+        os.remove(fileName)
+    except OSError:
+        pass
+    s1_pcap = S1.popen('tcpdump -i any -w '+fileName) # s1-eth1 eth0 any
+
+
     # s1_pcap.terminate()
-    # s1_pcap = S1.popen('tcpdump -w simple.pcap -i any')
     c1 = net.addController('c1',controller=RemoteController, ip="172.19.0.2",port=6633)
-    # c1 = net.addController('c1',controller=RemoteController, ip="127.0.0.1",port=6633)
-    # c2 = net.addController('c2',controller=RemoteController, ip="172.19.0.3",port=6633)
-    # c3 = net.addController('c3',controller=RemoteController, ip="172.19.0.4",port=6633)
-    for i in range(3):
+    c2 = net.addController('c2',controller=RemoteController, ip="172.19.0.3",port=6633)
+    c3 = net.addController('c3',controller=RemoteController, ip="172.19.0.4",port=6633)
+
+    # net.addLink(c1,S1)
+    for i in range(2):
         Hn = net.addHost('h' + str(i+1))
         net.addLink(Hn,S1)
     for h in net.hosts:
@@ -30,11 +34,25 @@ def emptyNet():
         h.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
         h.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
     net.start()
-    CLI.do_dpctl('dump-flows -O OpenFlow13')
+    
 
-    H1 = net.getNodeByName('h1')
-    for h in net.hosts:
-        net.ping([H1,h])
+    # CLI.do_dpctl('dump-flows -O OpenFlow13')
+    # H1 = net.getNodeByName('h1')
+    # for h in net.hosts:
+        # net.ping([H1,h])
+        # net.pingAll()
+    # sleep(5)
+    # s1_pcap = S1.popen('tcpdump -w '+fileName+' -i any')
+    for i in range(3):
+        # sleep(5)
+        net.pingAll()
+        sleep(1)
+    # sleep(3)
+    # net.iperf(hosts=net.hosts,
+    #         #   l4Type='UDP',
+    #           seconds=1,
+    #         #   udpBw='1M'
+    #           )
     # for i in range(3):
     #     Hn = net.addHost( 'h' + str(i+3))
     #     hlink = net.addLink(S1,Hn)
@@ -42,7 +60,7 @@ def emptyNet():
     #     Hn.configDefault(defaultRoute=Hn.defaultIntf())
     #     net.pingAll()
     #     sleep(1)
-    sleep(1)
+    # sleep(1)
     # for i in range(1):
     #     net.pingAll()
     #     # os.system("ping -c 1 127.0.0.1" ) # FLAG
@@ -51,9 +69,11 @@ def emptyNet():
     # for i in range(1):
     #     net.pingAll()
     #     # sleep(5)
+    sleep(3)
     s1_pcap.terminate()
-    CLI(net)
+    # CLI(net)
     net.stop()
+
 if __name__ == '__main__':
     setLogLevel('info')
     emptyNet()
