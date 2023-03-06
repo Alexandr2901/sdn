ddfa9bb0c11e94f7a9cd3e7e87b98acb674f3d12
Aleksandr Zhitov <zitov01@mail.ru>
2023-03-06 17:26:43 +0300

    no ip

--- opendaylight/ENTRYPOINT.sh
+++ opendaylight/ENTRYPOINT.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
+
+# arrVar=("AC" "TV" "Mobile" "Fridge" "Oven" "Blender")
+arrVar=()
+for ((i=1; i<=10;i++)); do
+    param=$(dig +short opendaylight$i)
+    # echo $param
+    arrVar+=($param)
+done
+echo "hi"
+
+for value in "${arrVar[@]}"
+do
+     echo $value
+done
 index=-1
-allThreads=(172.19.0.2 172.19.0.3 172.19.0.4)
+# allThreads=(172.19.0.2 172.19.0.3 172.19.0.4)
 # echo $allThreads
 param=$(hostname -i)
 # echo $param
-for i in "${!allThreads[@]}"; do
-    [[ "${allThreads[$i]}" = "${param}" ]] && index=$(($i+1))
+for i in "${!arrVar[@]}"; do
+    [[ "${arrVar[$i]}" = "${param}" ]] && index=$(($i+1))
 done
-string="${allThreads[*]}"
+string="${arrVar[*]}"
 # echo $string
 # echo /opt/odl/karaf-0.8.4/bin/configure_cluster.sh $index $string
 
